d8bf2d90784c5d774851ec453885cc368451eb07
Mike Bostock <mbostock@gmail.com>
2015-12-21 11:32:59 -0800

    Rename for clarity.

--- src/band.js
+++ src/band.js
@@ -4,8 +4,8 @@ import ordinal from "./ordinal";
 export default function band() {
   var scale = ordinal().unknown(undefined),
       domain = scale.domain,
-      range = scale.range,
-      extent = [0, 1],
+      ordinalRange = scale.range,
+      range = [0, 1],
       step = 0,
       round = false,
       paddingInner = 0,
@@ -15,16 +15,16 @@ export default function band() {
 
   function rescale() {
     var n = domain().length,
-        reverse = extent[1] < extent[0],
-        start = extent[reverse - 0],
-        stop = extent[1 - reverse],
+        reverse = range[1] < range[0],
+        start = range[reverse - 0],
+        stop = range[1 - reverse],
         step = (stop - start) / (n - paddingInner + 2 * paddingOuter);
     if (round) step = Math.floor(step), start = Math.round(start + (stop - start - (n - paddingInner) * step) / 2);
     else start += step * paddingOuter;
     var values = sequence(n).map(function(i) { return start + step * i; });
     step *= 1 - paddingInner;
     if (round) step = Math.round(step);
-    return range(reverse ? values.reverse() : values);
+    return ordinalRange(reverse ? values.reverse() : values);
   }
 
   scale.domain = function(_) {
@@ -32,11 +32,11 @@ export default function band() {
   };
 
   scale.range = function(_) {
-    return arguments.length ? (extent = [+_[0], +_[1]], rescale()) : extent.slice();
+    return arguments.length ? (range = [+_[0], +_[1]], rescale()) : range.slice();
   };
 
   scale.rangeRound = function(_) {
-    return extent = [+_[0], +_[1]], round = true, rescale();
+    return range = [+_[0], +_[1]], round = true, rescale();
   };
 
   scale.band = function() {
--- src/point.js
+++ src/point.js
@@ -4,8 +4,8 @@ import ordinal from "./ordinal";
 export default function point() {
   var scale = ordinal().unknown(undefined),
       domain = scale.domain,
-      range = scale.range,
-      extent = [0, 1],
+      ordinalRange = scale.range,
+      range = [0, 1],
       step = 0,
       round = false,
       padding = 0;
@@ -14,14 +14,14 @@ export default function point() {
 
   function rescale() {
     var n = domain().length,
-        reverse = extent[1] < extent[0],
-        start = extent[reverse - 0],
-        stop = extent[1 - reverse];
+        reverse = range[1] < range[0],
+        start = range[reverse - 0],
+        stop = range[1 - reverse];
     step = n < 2 ? (start = (start + stop) / 2, 0) : (stop - start) / (n - 1 + padding);
     if (round) step = Math.floor(step), start = Math.round(start + step * padding / 2 + (stop - start - (n - 1 + padding) * step) / 2);
     else start += step * padding / 2;
     var values = sequence(n).map(function(i) { return start + step * i; });
-    return range(reverse ? values.reverse() : values);
+    return ordinalRange(reverse ? values.reverse() : values);
   }
 
   scale.domain = function(_) {
@@ -29,11 +29,11 @@ export default function point() {
   };
 
   scale.range = function(_) {
-    return arguments.length ? (extent = [+_[0], +_[1]], rescale()) : extent.slice();
+    return arguments.length ? (range = [+_[0], +_[1]], rescale()) : range.slice();
   };
 
   scale.rangeRound = function(_) {
-    return extent = [+_[0], +_[1]], round = true, rescale();
+    return range = [+_[0], +_[1]], round = true, rescale();
   };
 
   scale.step = function() {
