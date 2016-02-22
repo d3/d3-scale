c9bca8ff70c40384bb807010d7c53eca493adf5a
Mike Bostock <mbostock@gmail.com>
2016-02-22 09:20:52 -0800

    Lazily construct interpolators.

--- src/rainbow.js
+++ src/rainbow.js
@@ -2,31 +2,22 @@ import {cubehelix} from "d3-color";
 import {interpolateCubehelixLong} from "d3-interpolate";
 import sequential from "./sequential";
 
-var a = cubehelix(-100, 0.75, 0.35),
-    b = cubehelix(80, 1.50, 0.8),
-    c = cubehelix(260, 0.75, 0.35),
-    d = cubehelix();
-
-var interpolateWarm = interpolateCubehelixLong(a, b),
-    interpolateCool = interpolateCubehelixLong(c, b);
-
-function interpolateRainbow(t) {
-  if (t < 0 || t > 1) t -= Math.floor(t);
-  var ts = Math.abs(t - 0.5);
-  d.h = 360 * t - 100;
-  d.s = 1.5 - 1.5 * ts;
-  d.l = 0.8 - 0.9 * ts;
-  return d + "";
-}
-
 export function warm() {
-  return sequential(interpolateWarm);
+  return sequential(interpolateCubehelixLong(cubehelix(-100, 0.75, 0.35), cubehelix(80, 1.50, 0.8)));
 }
 
 export function cool() {
-  return sequential(interpolateCool);
+  return sequential(interpolateCubehelixLong(cubehelix(260, 0.75, 0.35), cubehelix(80, 1.50, 0.8)));
 }
 
 export default function() {
-  return sequential(interpolateRainbow);
+  var rainbow = cubehelix();
+  return sequential(function(t) {
+    if (t < 0 || t > 1) t -= Math.floor(t);
+    var ts = Math.abs(t - 0.5);
+    rainbow.h = 360 * t - 100;
+    rainbow.s = 1.5 - 1.5 * ts;
+    rainbow.l = 0.8 - 0.9 * ts;
+    return rainbow + "";
+  });
 }
