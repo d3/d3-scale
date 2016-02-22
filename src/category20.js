1f4050f955eff8ba32ed69389d5e8337d35186f0
Mike Bostock <mbostock@gmail.com>
2016-02-22 09:13:32 -0800

    Static category ranges.

--- src/category20.js
+++ src/category20.js
@@ -1,6 +1,8 @@
 import colors from "./colors";
 import ordinal from "./ordinal";
 
+var category20 = colors("1f77b4aec7e8ff7f0effbb782ca02c98df8ad62728ff98969467bdc5b0d58c564bc49c94e377c2f7b6d27f7f7fc7c7c7bcbd22dbdb8d17becf9edae5");
+
 export default function() {
-  return ordinal().range(colors("1f77b4aec7e8ff7f0effbb782ca02c98df8ad62728ff98969467bdc5b0d58c564bc49c94e377c2f7b6d27f7f7fc7c7c7bcbd22dbdb8d17becf9edae5"));
+  return ordinal().range(category20);
 }
