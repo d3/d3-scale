67c7a58e1140b4088ab99872c809e19c32f94fc5
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-06-07 13:17:31 -0700

    Add ordinal invert.

--- src/ordinal.js
+++ src/ordinal.js
@@ -3,25 +3,26 @@ import {slice} from "./array";
 
 export var implicit = {name: "implicit"};
 
-export default function ordinal(range) {
+export default function ordinal() {
   var index = map(),
+      inverseIndex = null,
       domain = [],
+      range = [],
       unknown = implicit;
-
-  range = range == null ? [] : slice.call(range);
 
   function scale(d) {
     var key = d + "", i = index.get(key);
     if (!i) {
       if (unknown !== implicit) return unknown;
       index.set(key, i = domain.push(d));
+      inverseIndex = null;
     }
     return range[(i - 1) % range.length];
   }
 
   scale.domain = function(_) {
     if (!arguments.length) return domain.slice();
-    domain = [], index = map();
+    domain = [], index = map(), inverseIndex = null;
     var i = -1, n = _.length, d, key;
     while (++i < n) if (!index.has(key = (d = _[i]) + "")) index.set(key, domain.push(d));
     return scale;
@@ -33,6 +34,15 @@ export default function ordinal(range) {
 
   scale.unknown = function(_) {
     return arguments.length ? (unknown = _, scale) : unknown;
+  };
+
+  scale.invert = function(_) {
+    if (!inverseIndex) {
+      inverseIndex = map();
+      var n = domain.length;
+      while (--n >= 0) inverseIndex.set(range[n], n+1);
+    }
+    return domain[inverseIndex.get(_) - 1];
   };
 
   scale.copy = function() {
