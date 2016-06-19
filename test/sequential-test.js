339e7c61129b041a27bd9bdbb0ab683773c544b8
Curran <curran.kelleher@gmail.com>
2016-06-19 21:41:23 +0530

    Add failing test

--- test/sequential-test.js
+++ test/sequential-test.js
@@ -54,3 +54,14 @@ tape("sequential.copy() returns an isolated copy of the scale", function(test) {
   test.deepEqual(s1.clamp(), false);
   test.end();
 });
+
+tape("scaleSequential.interpolate(interpolate) takes a custom interpolator factory", function(test) {
+  function interpolate(t) { return t * 2; }
+  var s = scale.scaleSequential(function(t) { return t; })
+    .interpolate(interpolate);
+  test.equal(s.interpolate(), interpolate);
+  test.equal(s(-0.5), -1);
+  test.equal(s( 0.0),  0.0);
+  test.equal(s( 0.5),  1);
+  test.end();
+});
