396d1c95fef85241bc3b4b75d747174251ae8e89
Mike Bostock <mbostock@gmail.com>
2017-05-15 12:03:52 -0700

    Fix #81 - use d3.tickIncrement for linear.nice.

--- src/linear.js
+++ src/linear.js
@@ -1,4 +1,4 @@
-import {ticks, tickStep} from "d3-array";
+import {ticks, tickIncrement} from "d3-array";
 import {interpolateNumber as reinterpolate} from "d3-interpolate";
 import {default as continuous, copy, deinterpolateLinear as deinterpolate} from "./continuous";
 import tickFormat from "./tickFormat";
@@ -16,19 +16,39 @@ export function linearish(scale) {
   };
 
   scale.nice = function(count) {
-    var d = domain(),
-        i = d.length - 1,
-        n = count == null ? 10 : count,
-        start = d[0],
-        stop = d[i],
-        step = tickStep(start, stop, n);
+    if (count == null) count = 10;
 
-    if (step) {
+    var d = domain(),
+        i0 = 0,
+        i1 = d.length - 1,
+        start = d[i0],
+        stop = d[i1],
+        step;
+
+    if (stop < start) {
+      step = start, start = stop, stop = step;
+      step = i0, i0 = i1, i1 = step;
+    }
+
+    step = tickIncrement(start, stop, count);
+
+    if (step > 0) {
       start = Math.floor(start / step) * step;
       stop = Math.ceil(stop / step) * step;
-      step = tickStep(start, stop, n);
-      d[0] = Math.floor(start / step) * step;
-      d[i] = Math.ceil(stop / step) * step;
+      step = tickIncrement(start, stop, count);
+    } else if (step < 0) {
+      start = Math.ceil(start * step) / step;
+      stop = Math.floor(stop * step) / step;
+      step = tickIncrement(start, stop, count);
+    }
+
+    if (step > 0) {
+      d[i0] = Math.floor(start / step) * step;
+      d[i1] = Math.ceil(stop / step) * step;
+      domain(d);
+    } else if (step < 0) {
+      d[i0] = Math.ceil(start * step) / step;
+      d[i1] = Math.floor(stop * step) / step;
       domain(d);
     }
 
