da99948d2fb7a4b7a93fc69be427e5cd7550c8a3
Dominik Moritz <domoritz@gmail.com>
2020-06-15 08:16:26 -0700

    Use var

--- src/linear.js
+++ src/linear.js
@@ -4,29 +4,29 @@ import {initRange} from "./init.js";
 import tickFormat from "./tickFormat.js";
 
 export function linearish(scale) {
-  const domain = scale.domain;
+  var domain = scale.domain;
 
   scale.ticks = function(count) {
-    const d = domain();
+    var d = domain();
     return ticks(d[0], d[d.length - 1], count == null ? 10 : count);
   };
 
   scale.tickFormat = function(count, specifier) {
-    const d = domain();
+    var d = domain();
     return tickFormat(d[0], d[d.length - 1], count == null ? 10 : count, specifier);
   };
 
   scale.nice = function(count) {
     if (count == null) count = 10;
 
-    const d = domain();
-    let i0 = 0;
-    let i1 = d.length - 1;
-    let start = d[i0];
-    let stop = d[i1];
-    let prestep;
-    let step;
-    let maxIter = 10;
+    var d = domain();
+    var i0 = 0;
+    var i1 = d.length - 1;
+    var start = d[i0];
+    var stop = d[i1];
+    var prestep;
+    var step;
+    var maxIter = 10;
 
     if (stop < start) {
       step = start, start = stop, stop = step;
@@ -34,7 +34,7 @@ export function linearish(scale) {
     }
     
     while (maxIter-- > 0) {
-      const step = tickIncrement(start, stop, count);
+      step = tickIncrement(start, stop, count);
       if (step === prestep) {
         d[i0] = start
         d[i1] = stop
@@ -58,7 +58,7 @@ export function linearish(scale) {
 }
 
 export default function linear() {
-  const scale = continuous();
+  var scale = continuous();
 
   scale.copy = function() {
     return copy(scale, linear());
