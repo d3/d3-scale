f3552d86a0479db869bec57b95a62b782f875a8b
michielmetcake <17408017+michielmetcake@users.noreply.github.com>
2020-07-29 11:37:19 +0200

    Update utcTime.js

--- src/utcTime.js
+++ src/utcTime.js
@@ -3,6 +3,6 @@ import {utcFormat} from "d3-time-format";
 import {utcYear, utcMonth, utcWeek, utcDay, utcHour, utcMinute, utcSecond, utcMillisecond} from "d3-time";
 import {initRange} from "./init.js";
 
-export default function() {
+export default function utcTime() {
   return initRange.apply(calendar(utcYear, utcMonth, utcWeek, utcDay, utcHour, utcMinute, utcSecond, utcMillisecond, utcFormat).domain([Date.UTC(2000, 0, 1), Date.UTC(2000, 0, 2)]), arguments);
 }
